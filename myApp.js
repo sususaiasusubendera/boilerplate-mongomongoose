0ec05e73c5f171a83a246d405c3ed33221cf293f
sususaiasusubendera <oplessnewbee@gmail.com>
2024-04-23 18:11:01 +0700

    PASS - change sample.env to .env, install package and import mongoose

--- myApp.js
+++ myApp.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 
+const mongoose = require('mongoose');
 
 let Person;
 
@@ -50,6 +51,8 @@ const queryChain = (done) => {
 
   done(null /*, data*/);
 };
+
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 /** **Well Done !!**
 /* You completed these challenges, let's go celebrate !
