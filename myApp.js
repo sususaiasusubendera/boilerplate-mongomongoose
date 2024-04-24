be354a520dd7136b0ee8512834e24e7be373af82
sususaiasusubendera <oplessnewbee@gmail.com>
2024-04-24 22:21:59 +0700

    edit package.json scripts and PASS until #5

--- myApp.js
+++ myApp.js
@@ -1,33 +1,88 @@
 require('dotenv').config();
-
 const mongoose = require('mongoose');
 
-let Person;
+/* 2. create a model */
+const personSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  age: {
+    type: Number
+  },
+  favoriteFoods: {
+    type: [String]
+  }
+});
 
+let Person = mongoose.model("Person", personSchema);
+
+/* 3. create and save a record of a model */
 const createAndSavePerson = (done) => {
-  done(null /*, data*/);
+  var luffy = new Person({
+    name: "Luffy",
+    age: 20,
+    favoriteFoods: ["Meat", "Fish"]
+  });
+
+  luffy.save((err, data) => {
+    if (err) return console.error(err);
+    done(null, data);
+  });
 };
+
+/* 4. create many records with model.create() */
+// var arrayOfPeople = [
+//   {name: "A", age: 21, favoriteFoods: ["Apple"]},
+//   {name: "B", age: 22, favoriteFoods: ["Banana"]},
+//   {name: "C", age: 23, favoriteFoods: ["Carrot"]}
+// ];
 
 const createManyPeople = (arrayOfPeople, done) => {
-  done(null /*, data*/);
+    Person.create(arrayOfPeople, (err, data) => {
+      if (err) return console.error(err);
+      done(null, data);
+    });
 };
 
+/* 5. use model.find to search your database */
 const findPeopleByName = (personName, done) => {
-  done(null /*, data*/);
+  Person.find({name: personName}, (err, data) => {
+    if (err) console.error(err);
+    done(null, data);
+  })
 };
 
+/* 6. use model.findOne() to return a single matching document from your database */
 const findOneByFood = (food, done) => {
-  done(null /*, data*/);
+  Person.findOne({favoriteFoods: food}, (err, data) => {
+    if (err) console.error(err);
+    done(null, data);
+  })
 };
 
+/* 6. use model.findById() to search your database by _id */
 const findPersonById = (personId, done) => {
-  done(null /*, data*/);
+  Person.findById({_id: personId}, (err, data) => {
+    if (err) console.error(err);
+    done(null, data);
+  })
 };
 
+/* 6. perform classic updates by running find, edit, then save */
 const findEditThenSave = (personId, done) => {
   const foodToAdd = "hamburger";
 
-  done(null /*, data*/);
+  Person.findById({_id: personId}, (err, data) => {
+    if (err) return console.error(err);
+
+    data.favoriteFoods.push(foodToAdd);
+
+    data.save((err, updatedData) => {
+      if (err) console.error(err);
+    done(null, updatedData);
+    })
+  })
 };
 
 const findAndUpdate = (personName, done) => {
